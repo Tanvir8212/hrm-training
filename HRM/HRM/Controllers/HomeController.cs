203e5451399e6c017d150c6cc9b143638079663f
sajid8212 <sajid8212@gmail.com>
2019-04-20 22:05:14 +0600

    Show tables with jQuery DataTables

--- HRM/HRM/Controllers/HomeController.cs
+++ HRM/HRM/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HRM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,16 @@ namespace HRM.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
+            ViewBag.Departments = db.Departments.ToList();
+            ViewBag.Divisions = db.Divisions.ToList();
+            ViewBag.Designations = db.Designations.ToList();
+            ViewBag.Employees = db.Employees.ToList();
+            ViewBag.Sections = db.Sections.ToList();
+            
             return View();
         }
 
